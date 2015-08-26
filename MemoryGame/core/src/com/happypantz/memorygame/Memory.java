c080a86dcea1e9f10de2921a5fd98b2a8e5d45b7
Higles <josephhigley@gmail.com>
2015-08-26 15:53:23 -0700

    got rid of batch and replaced with stage

--- MemoryGame/core/src/com/happypantz/memorygame/Memory.java
+++ MemoryGame/core/src/com/happypantz/memorygame/Memory.java
@@ -6,17 +6,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class Memory extends Game {
-	SpriteBatch batch;
-	Texture img;
+	Stage stage;
 
 	float r, g, b;
 	int flagR, flagG, flagB;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
+		stage = new Stage();
+
 		r = 1;
 		g = 0.5f;
 		b = 0;
@@ -30,6 +33,8 @@ public class Memory extends Game {
 		Gdx.gl.glClearColor(r, g, b, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		Gdx.input.setInputProcessor(stage);
+
 		if (r <= 0) flagR = 1;
 		else if (r >= 1) flagR = -1;
 		r += flagR * 0.01;
@@ -42,8 +47,7 @@ public class Memory extends Game {
 		else if (b >= 1) flagB = -1;
 		b += flagB * 0.01;
 
-		batch.begin();
-
-		batch.end();
+		stage.act();
+		stage.draw();
 	}
 }
