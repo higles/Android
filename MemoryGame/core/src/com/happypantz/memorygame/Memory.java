f55d2706c09eb2740d7e4a6e9763a3f917d2c33f
Higles <josephhigley@gmail.com>
2015-08-27 14:46:11 -0700

    example of how to use actors in a stage

--- MemoryGame/core/src/com/happypantz/memorygame/Memory.java
+++ MemoryGame/core/src/com/happypantz/memorygame/Memory.java
@@ -1,20 +1,18 @@
 package com.happypantz.memorygame;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class Memory extends Game {
 	Stage stage;
-	SpriteBatch batch;
-	Sprite sprite;
+
+	Image img;
 
 	float r, g, b;
 	int flagR, flagG, flagB;
@@ -22,10 +20,9 @@ public class Memory extends Game {
 	@Override
 	public void create () {
 		stage = new Stage();
-		batch = new SpriteBatch();
-		sprite = new Sprite(new Texture(Gdx.files.internal("textures/bot_left_up_square.png")));
-		sprite.setSize(100, 100);
-		sprite.setPosition(100, 100);
+		img = new Image(new Texture(Gdx.files.internal("textures/bot_left_up_square.png")));
+		img.setSize(100, 100);
+		img.setPosition(100, 100);
 
 		r = 1;
 		g = 0.5f;
@@ -33,6 +30,8 @@ public class Memory extends Game {
 		flagR = 1;
 		flagG = 1;
 		flagB = 1;
+
+		stage.addActor(img);
 	}
 
 	@Override
@@ -54,9 +53,6 @@ public class Memory extends Game {
 		else if (b >= 1) flagB = -1;
 		b += flagB * 0.01;
 
-		batch.begin();
-			sprite.draw(batch);
-		batch.end();
 		stage.act();
 		stage.draw();
 	}
