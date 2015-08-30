319d62b92be8f8428a4341b8add299820f2b17f3
chaseguyer <chaseguyer@gmail.com>
2015-08-29 20:30:57 -0700

    userInerface is integrated.

--- MemoryGame/core/src/com/happypantz/memorygame/Memory.java
+++ MemoryGame/core/src/com/happypantz/memorygame/Memory.java
@@ -5,53 +5,56 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
 public class Memory extends Game {
 	Stage stage;
 
-	Image img;
-
-	float r, g, b;
-	int flagR, flagG, flagB;
+	ImageButton[] img = new ImageButton[4];
+	Skin skin = new Skin();
 	
 	@Override
 	public void create () {
 		stage = new Stage();
-		img = new Image(new Texture(Gdx.files.internal("textures/bot_left_up_square.png")));
-		img.setSize(100, 100);
-		img.setPosition(100, 100);
+		skin = 	new Skin(Gdx.files.internal("skins/skin.json"),
+				new TextureAtlas(Gdx.files.internal("skins/memory.pack"))
+		);
 
-		r = 1;
-		g = 0.5f;
-		b = 0;
-		flagR = 1;
-		flagG = 1;
-		flagB = 1;
+		float screenWidth = Gdx.graphics.getWidth();
+		float screenHeight = Gdx.graphics.getHeight();
 
-		stage.addActor(img);
+		img[0] = new ImageButton(skin, "bot_left");
+		img[1] = new ImageButton(skin, "bot_right");
+		img[2] = new ImageButton(skin, "top_left");
+		img[3] = new ImageButton(skin, "top_right");
+
+		for(int i = 0; i < img.length; i++) {
+			img[i].setSize(screenWidth / 2, screenHeight / 2);
+			//img[i].setPosition(((i % 2)*(screenWidth/2)), ((i / 2)*(screenHeight/2)));
+
+			stage.addActor(img[i]);
+		}
+
+		img[0].setPosition(0 , 0);
+		img[1].setPosition(screenWidth / 2, 0);
+		img[2].setPosition(0, screenHeight / 2);
+		img[3].setPosition(screenWidth / 2, screenHeight / 2);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(r, g, b, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		Gdx.input.setInputProcessor(stage);
 
-		if (r <= 0) flagR = 1;
-		else if (r >= 1) flagR = -1;
-		r += flagR * 0.01;
-
-		if (g <= 0) flagG = 1;
-		else if (g >= 1) flagG = -1;
-		g += flagG * 0.01;
-
-		if (b <= 0) flagB = 1;
-		else if (b >= 1) flagB = -1;
-		b += flagB * 0.01;
+	//	Drawable temp = img[2].getStyle().imageUp;
+	//	img[2].getStyle().imageUp = img[2].getStyle().imageDown;
 
 		stage.act();
 		stage.draw();
